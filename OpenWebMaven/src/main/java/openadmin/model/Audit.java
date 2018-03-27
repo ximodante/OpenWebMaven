27ac766df7d3928d8fca25588a3ed85a8a3421c1
ximodante <ximodante@gmail.com>
2018-03-27 21:00:39 +0200

    003 A vore si va

--- OpenWebMaven/src/main/java/openadmin/model/Audit.java
+++ OpenWebMaven/src/main/java/openadmin/model/Audit.java
@@ -14,7 +14,7 @@ public class Audit {
 	@Getter @Setter
 	private String lastUser;
 	@Getter @Setter
-	private LocalDate data;
+	private LocalDate data ;
 	
 	public void setChanges(String user) {
 		this.lastUser=user;
